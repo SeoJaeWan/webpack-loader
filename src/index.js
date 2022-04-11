cca654d7d164f0e1cdb1fe7e2afd167d1fb93e67
Ing-yeo_Developer <sjw7324@naver.com>
2022-04-11 14:31:37 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h3>Webpack example</h3>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
